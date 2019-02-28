af89ed7ebde60d8928bf71f6b53dc6b81a374975
hemapotha <47896862+hemapotha@users.noreply.github.com>
2019-02-28 10:32:19 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,15 @@
+List = [1,2,3,4,5,6]
+print (List)
+print (List[1])
+for i in List:
+	print (i)
+for i in range(0,6):
+	print(i)
+List[1]=10
+print(List)	
+print(len(List))
+if "4" in List:
+	print("yes")
+else:
+	print("no")	
+
