ba5dfe92baf227a2a14899b90d416714d481b984
hemapotha <47896862+hemapotha@users.noreply.github.com>
2019-03-07 08:48:50 +0530

    Add files via upload

---
+++ ltn.py
@@ -0,0 +1,9 @@
+a=int(intput('enter a number:'))
+b=int(intput('enter a number:'))
+c=int(intput('enter a number:'))
+if(a>b & a>c):
+	print('a is the largest number!')
+elif(b>c):
+	print('b is the largest!')
+else:
+	print('c is the largest!')
